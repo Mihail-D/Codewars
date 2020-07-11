e6e7a123f4cc0f22889c6ff0104b613e5855d664
Mihail-D <nuby.4register@gmail.com>
2020-07-11 21:10:01 +0300

    +

---
+++ abbreviateTwoWordName.js
@@ -0,0 +1,8 @@
+function abbrevName(name) {
+  name = name.split(" ");
+  return (
+    name[0].charAt(0).toUpperCase() + "." + name[1].charAt(0).toUpperCase()
+  );
+}
+
+console.log(abbrevName("Sam Harris"));
